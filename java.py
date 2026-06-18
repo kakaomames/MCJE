89328e39d4d332398c5615ef208136288934955e
kakaomame <nattyannkunndesu@icloud.com>
2026-06-18 15:27:44 +0900

    Update java.py

--- java.py
+++ java.py
@@ -72,7 +72,7 @@ path_separator = ";" if platform.system() == "Windows" else ":"
 classpath = f"{client_jar}{path_separator}{libraries_dir}"
 
 # Javaコマンドのベース作成
-cmd = ["java", "-Xmx2G", "-cp", classpath, "net.minecraft.client.main.Main"]
+cmd = ["java", "-Xmx2G", "-cp", classpath, "net.minecraft.client.main.Main","--username",PLAYER_NAME,"--version",VERSION,"--gameDir",LAN_MINECRAFT_DIR,"--assetsDir",assets_dir]
 
 # 引数の解析と追加
 if "arguments" in data and "game" in data["arguments"]:
