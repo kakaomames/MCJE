90b0f01bf2d9c7a4eb0efdf12663eab4d06e3ded
kakaomame <nattyannkunndesu@icloud.com>
2026-06-18 15:21:33 +0900

    Update java.py

--- java.py
+++ java.py
@@ -37,7 +37,7 @@ print(f"Device-Based Hardware UUID: {AUTH_UUID}")
 json_path = "Minecraft/versions/26.2/26.2.json"
 client_jar = "release/26.2.jar"
 libraries_dir = "lib/*"
-assets_dir = "assets/
+assets_dir = "assets/"
 
 
 # 44行目の「with open...」の直前にこれを貼り付けてください
